e45fad087b88e7b68fe1798f4b23a659a3f58fd0
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-24 00:12:35 -0700

    Create invoice items model, factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124065417) do
+ActiveRecord::Schema.define(version: 20170124071051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20170124065417) do
   create_table "customers", force: :cascade do |t|
     t.text     "first_name"
     t.text     "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "invoice_id"
+    t.integer  "quantity"
+    t.integer  "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/invoice_items.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :invoice_item do
+    item_id 1
+    invoice_id 1
+    quantity 1
+    unit_price 1
+    created_at "2017-01-24 00:10:51"
+    updated_at "2017-01-24 00:10:51"
+  end
+end
