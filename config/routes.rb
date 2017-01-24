c24b45418b47a6c20b86644deaf0ff35aa1dee25
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-23 23:57:50 -0700

    Add items index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :customers, only: [:index]
       resources :transactions, only: [:index]
       resources :invoices, only: [:index]
+      resources :items, only: [:index]
     end
   end
 end
