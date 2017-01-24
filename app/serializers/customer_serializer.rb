7b96b9d54556629813bd1d30e8ac0e80cc1deec3
Amz K <akintner@u.rochester.edu>
2017-01-24 11:55:26 -0700

    update all serializers for tests to pass

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,3 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name
+  attributes :id, :first_name, :last_name, :created_at, :updated_at
 end
--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,3 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :credit_card_number, :result
+  attributes :id, :credit_card_number, :result, :created_at, :updated_at
 end
