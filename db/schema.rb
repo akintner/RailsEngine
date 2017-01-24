8d4c60db0348d0c12d37dffedb7da048cbc8bd09
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-23 23:56:42 -0700

    WIP - add items model and factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124063638) do
+ActiveRecord::Schema.define(version: 20170124065417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20170124063638) do
     t.integer  "customer_id"
     t.integer  "merchant_id"
     t.string   "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "unit_price"
+    t.integer  "merchant_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
