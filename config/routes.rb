c9a7f9d99e278156483850a0935d9290ed640450
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-24 11:34:17 -0700

    Add invoice item serializer and show endpoint. Add missing serializer for item

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :transactions, only: [:index]
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
-      resources :invoice_items, only: [:index]
+      resources :invoice_items, only: [:index, :show]
     end
   end
 end
