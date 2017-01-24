b0a2641cfea54cca0c9f9badcd5d28dd1602b17b
Amz K <akintner@u.rochester.edu>
2017-01-24 11:06:11 -0700

    add show page to merchants, customers, transactions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do 
     namespace :v1 do 
-      resources :merchants, only: [:index]
-      resources :customers, only: [:index]
-      resources :transactions, only: [:index]
+      resources :merchants, only: [:index, :show]
+      resources :customers, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
       resources :invoices, only: [:index]
       resources :items, only: [:index]
       resources :invoice_items, only: [:index]
