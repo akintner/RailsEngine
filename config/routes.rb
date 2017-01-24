cf5c993ab67a054f7b29b5d764f5fff058310d68
Amz K <akintner@u.rochester.edu>
2017-01-23 19:58:14 -0700

    add controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json } do 
     namespace :v1 do 
       resources :merchants, only: [:index]
+      resources :customers, only: [:index]
     end
   end
 end
