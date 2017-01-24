cbd01eea97bf6036dad79764f7e660a982d88837
Amz K <akintner@u.rochester.edu>
2017-01-24 16:04:25 -0700

    write find_all spec for customers, all tests passing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,4 +14,21 @@ class Customer < ApplicationRecord
       nil
     end     
   end
+
+  def self.where_by_params(param)
+     if param["first_name"]
+      find_by(first_name: param["first_name"])
+    elsif param["id"]
+      find_by(id: param["id"].to_i)
+    elsif param["last_name"]
+      find_by(last_name: param["last_name"])
+    elsif param["created_at"]
+      find_by(created_at: param["created_at"])
+    elsif param["updated_at"]
+      find_by(updated_at: param["updated_at"])
+    else
+      nil
+    end 
+  end
+   
 end
