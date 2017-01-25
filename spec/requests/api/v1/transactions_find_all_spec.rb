5fe5e0c90f579f9d2e8e02cca347f8aa8506b9a0
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-25 05:42:45 -0700

    Add transactions model test, covering validation and methods. Fix incorrect activrecord method within where_by_params method within transactions model.

--- spec/requests/api/v1/transactions_find_all_spec.rb
+++ spec/requests/api/v1/transactions_find_all_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe "Transactions API Find_All Controller" do
     get '/api/v1/transactions/find_all?result=success'
 
     transactions = JSON.parse(response.body)
+    transaction = transactions.first
 
     expect(response).to be_success
-    expect(transactions).to have_key("id")
-    expect(transactions).to have_key("result")
-    expect(transactions).to have_key("credit_card_number")
-    expect(transactions["result"]).to eq("success")
+    expect(transaction).to have_key("id")
+    expect(transaction).to have_key("result")
+    expect(transaction).to have_key("credit_card_number")
+    expect(transaction["result"]).to eq("success")
   end
 
   it "can find all transactions by credit_card_number" do 
@@ -19,11 +20,12 @@ RSpec.describe "Transactions API Find_All Controller" do
     get '/api/v1/transactions/find_all?credit_card_number=4654405418249632'
 
     transactions = JSON.parse(response.body)
+    transaction = transactions.first
 
     expect(response).to be_success
-    expect(transactions).to have_key("id")
-    expect(transactions).to have_key("result")
-    expect(transactions).to have_key("credit_card_number")
-    expect(transactions["credit_card_number"]).to eq("4654405418249632")
+    expect(transaction).to have_key("id")
+    expect(transaction).to have_key("result")
+    expect(transaction).to have_key("credit_card_number")
+    expect(transaction["credit_card_number"]).to eq("4654405418249632")
   end
 end
