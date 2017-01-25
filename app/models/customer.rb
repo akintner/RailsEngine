4530b963b0914f4d2fba2958c4c318cd5fbcc24a
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-25 05:16:50 -0700

    Add customers model test covering validation and methods. Correct activerecord from find to where within where_by_params of customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -17,15 +17,15 @@ class Customer < ApplicationRecord
 
   def self.where_by_params(param)
      if param["first_name"]
-      find_by(first_name: param["first_name"])
+      where(first_name: param["first_name"])
     elsif param["id"]
-      find_by(id: param["id"].to_i)
+      where(id: param["id"].to_i)
     elsif param["last_name"]
-      find_by(last_name: param["last_name"])
+      where(last_name: param["last_name"])
     elsif param["created_at"]
-      find_by(created_at: param["created_at"])
+      where(created_at: param["created_at"])
     elsif param["updated_at"]
-      find_by(updated_at: param["updated_at"])
+      where(updated_at: param["updated_at"])
     else
       nil
     end 
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -14,7 +14,41 @@ RSpec.describe Customer, type: :model do
     end
   end
   describe 'methods' do
-    xit 'can return a random customer' do
+    it 'can find the first match based on any attribute' do
+      test_customers = create_list(:customer, 3)
+      test_params = {"first_name" => "Prof. Severus"}
+
+      find_customer = Customer.find_by_params(test_params)
+
+      expect(find_customer.id).to eq(test_customers.first.id)
     end
+
+    it 'can find all matches based on any attribute' do
+      test_customers = create_list(:customer, 3)
+      test_customers[0].update(last_name: 'Snape')
+      test_customers[1].update(last_name: 'SomethingElse')
+      test_customers[2].update(last_name: 'Snape')
+      test_params = {"last_name" => "Snape"}
+
+      find_customers = Customer.where_by_params(test_params)
+
+      expect(find_customers.count).to eq(2)
+    end
+
+    xit 'can return a random customer' do
+      create_list(:customer, 100)
+
+      random_customer1 = Customer.random
+      random_customer2 = Customer.random
+
+      retry_count = 0
+      while random_customer1.id.eql?(random_customer2.id) && retry_count < 5
+        retry_count =+ 1
+        random_customer2 = Customer.random
+      end
+
+      expect(random_customer1.id.eql?(random_customer2.id)).to be_falsy
+    end
+
   end
 end
