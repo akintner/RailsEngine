ff4af1095ffae07bcea51189199869db49dfcdc9
Amz K <akintner@u.rochester.edu>
2017-01-24 21:16:42 -0700

    add model test for associations on items, invoices, invoice_items

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,6 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
   attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  belongs_to :item 
+  belongs_to :invoice 
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do 
+    it  {should respond_to(:item) }
+    it  {should respond_to(:item) }
+  end
 end
