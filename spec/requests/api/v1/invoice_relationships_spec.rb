2c505d21e9fa837c2ae93ad49eb2a375239c83a8
Amz K <akintner@u.rochester.edu>
2017-01-25 16:54:13 -0700

    update to invoice_relationships_spec

--- spec/requests/api/v1/invoice_relationships_spec.rb
+++ spec/requests/api/v1/invoice_relationships_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'Invoices Relationships' do
+RSpec.describe 'Invoice Relationships' do
   it 'can find all transactions for invoice' do
     invoice = create(:invoice)
     transactions = create_list(:transaction, 4)
     invoice.transactions << transactions
     get "/api/v1/invoices/#{invoice.id}/transactions"
 
-    invoices = JSON.parse(response.body)
+    transactions = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(transactions.count).to eq(invoice.transactions.count)
@@ -19,7 +19,7 @@ RSpec.describe 'Invoices Relationships' do
     invoice.invoice_items << invoice_items   
     get "/api/v1/invoices/#{invoice.id}/invoice_items"
 
-    invoices = JSON.parse(response.body)
+    invoice_items = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(invoice_items.count).to eq(invoice.invoice_items.count)
@@ -31,7 +31,7 @@ RSpec.describe 'Invoices Relationships' do
     invoice.items << items    
     get "/api/v1/invoices/#{invoice.id}/items"
 
-    invoices = JSON.parse(response.body)
+    items = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(items.count).to eq(invoice.items.count)
@@ -39,24 +39,22 @@ RSpec.describe 'Invoices Relationships' do
 
   it 'can find customer for invoice' do
     invoice = create(:invoice)
-    customer = invoice.customer    
     get "/api/v1/invoices/#{invoice.id}/customer"
 
-    invoices = JSON.parse(response.body)
+    customer = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(customer.id).to eq(invoice.customer_id)
+    expect(customer["id"]).to eq(invoice.customer_id)
   end
 
   it 'can find merchant for invoice' do
     invoice = create(:invoice)
-    merchant = invoice.merchant   
     get "/api/v1/invoices/#{invoice.id}/merchant"
 
-    invoices = JSON.parse(response.body)
+    merchant = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(merchant.id).to eq(invoice.merchant_id)
+    expect(merchant["id"]).to eq(invoice.merchant_id)
   end
 
 end
