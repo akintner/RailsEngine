bd6d6684a6137c8ac18fba6d8bca5ac32e804c04
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-25 04:50:34 -0700

    Add invoice item model test covering validation and methods

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'can create Customer with all attributes, including timestamps from CSVs' do
+      test_customer = Customer.create(first_name: 'FirstName',
+                                      last_name: 'LastName',
+                                      created_at: '2017-01-24 00:10:51',
+                                      updated_at: '2017-01-24 00:10:51')
+
+      expect(test_customer).to be_valid
+      expect(Customer.count).to eq(1)
+      expect(Customer.first).to eq(test_customer)
+    end
+  end
+  describe 'methods' do
+    xit 'can return a random customer' do
+    end
+  end
 end
