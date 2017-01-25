9642642af7299f25975b09591820d20a16b8ab28
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-25 04:11:04 -0700

    Add random endpoint to invoices

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       namespace :invoices do
         get "/find" => "find#index"
         get "/find_all" => "find_all#index"
+        get "/random" => "random#index"
       end
       namespace :invoice_items do
         get "/find" => "find#index"
