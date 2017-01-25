70a6a6bc206a81f0a1304018ede2dbe4d3579856
Amz K <akintner@u.rochester.edu>
2017-01-25 15:42:29 -0700

    update spec and update item serializer

--- spec/requests/api/v1/item_relationships_spec.rb
+++ spec/requests/api/v1/item_relationships_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Items Relationships' do
     item.invoice_items << invoice_items
     get "/api/v1/items/#{item.id}/invoice_items"
 
-    items = JSON.parse(response.body)
+    invoice_items = JSON.parse(response.body)
 
     expect(response).to be_success
     expect(invoice_items.count).to eq(item.invoice_items.count)
@@ -15,12 +15,11 @@ RSpec.describe 'Items Relationships' do
 
   it 'can find merchant for item' do
     item = create(:item)
-    merchant = item.merchant
     get "//api/v1/items/#{item.id}/merchant"
 
-    items = JSON.parse(response.body)
+    merchant = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(merchant.id).to eq(item.merchant_id)
+    expect(merchant["id"]).to eq(item.merchant_id)
   end
 end
