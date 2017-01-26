f7ed6e1b44fb5206f3d81cc5cf9725bcc93602cb
Amz K <akintner@u.rochester.edu>
2017-01-26 16:48:19 -0700

    finalize all item BI and spec, everything passing in local tests and harness

--- spec/requests/api/v1/items_business_spec.rb
+++ spec/requests/api/v1/items_business_spec.rb
@@ -16,24 +16,23 @@ RSpec.describe 'Items Business Intelligence' do
     expect(response).to be_success
   end
 
-  it 'can find top x items by most sold' do
+  it "returns the top x items ranked by total sold" do
     #returns the top x item instances ranked by total number sold
-    item1 = create(:item)
-    item2 = create(:item)
-    item3 = create(:item)
-    invoice_1, invoice_2, invoice_3 = create_list(:invoice, 3)
-    create(:invoice_item, item: item1, invoice: invoice_1, quantity: 7)
-    create(:invoice_item, item: item2, invoice: invoice_2, quantity: 11)
-    create(:invoice_item, item: item3, invoice: invoice_3, quantity: 9)
+    item_1, item_2, item_3 = create_list(:item, 3)
+    invoice = create(:invoice)
+    create_list(:transaction, 3, invoice: invoice, result: 'success')
+    create(:invoice_item, item: item_1, invoice: invoice, quantity: 7)
+    create(:invoice_item, item: item_2, invoice: invoice, quantity: 19)
+    create(:invoice_item, item: item_3, invoice: invoice, quantity: 11)
 
     get "/api/v1/items/most_items?quantity=3"
 
     result = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(result).to have_key("item1")
-    expect(result).to have_key("item2")
-    expect(result).to have_key("item3")
+    expect(result.count).to eq(3)
+    expect(result.first["id"]).to eq(item_2.id)
+    expect(result.last["id"]).to eq(item_1.id)
   end
 
   it 'can find top x items by total revenue generated' do
