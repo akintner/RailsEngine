b01dbef34daa4d3c10c8acdf54c70fa46b6fa7c1
Laszlo Balogh <laszlo@thebaloghs.com>
2017-01-26 21:30:16 -0700

    Add created_at and updated_at to merchants and transactions factories

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :merchant do 
     name "Hogwarts Express Railways" 
+    created_at "2017-01-23 23:54:17"
+    updated_at "2017-01-23 23:54:17"
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,5 +3,7 @@ FactoryGirl.define do
     invoice
     credit_card_number "4654405418249632"
     result "success"
+    created_at "2017-01-23 23:54:17"
+    updated_at "2017-01-23 23:54:17"
   end
 end
